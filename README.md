a76b369fb5d0d34365a3270df357a7d05e94d3ef
unknown <caiobhadriano@gmail.com>
2022-11-15 22:06:32 -0300

    Criando o header da aplicação

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Projeto CRUD com Angular 9
+
+- Para iniciar o projeto primeiro execute o comando npm install na pasta backend e no frontend
+- Para rodar a API do backend basta executar o comando npm start
+- Execute o comando ng serve para rodar o frontend automaticamente com o angular
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/template/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
