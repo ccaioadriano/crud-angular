a2003387fba68b6deac967dd35176767dbf0e5dc
unknown <caiobhadriano@gmail.com>
2022-11-23 18:11:26 -0300

    Componente de inicio (Home)

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
@@ -12,6 +13,8 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { HeaderComponent } from './components/template/header/header.component';
 import { NavComponent } from './components/template/nav/nav.component';
+import { RedDirective } from './directives/red.directive';
+import { HomeComponent } from './views/home/home.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +22,8 @@ import { NavComponent } from './components/template/nav/nav.component';
     HeaderComponent,
     FooterComponent,
     NavComponent,
-
+    RedDirective,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -28,7 +32,8 @@ import { NavComponent } from './components/template/nav/nav.component';
     MatIconModule,
     MatToolbarModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
